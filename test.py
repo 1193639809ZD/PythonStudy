5f4d0a221842b2d56efbd918cec10676af380f64
eveleaf <eveleaf@outlook.com.ar>
2024-07-30 08:41:20 +0800

    test

--- test.py
+++ test.py
@@ -1,30 +1,10 @@
-from torch import nn
+"""
+Author: eveleaf eveleaf@outlook.com.ar
+Date: 2024-07-30 08:26:30
+LastEditors: eveleaf eveleaf@outlook.com.ar
+LastEditTime: 2024-07-30 08:40:26
+FilePath: /PythonStudy/test.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+"""
 
-
-class MyNet(nn.Module):
-    def __init__(self):
-        super(MyNet, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3)
-        self.conv2 = nn.Conv2d(64, 64, 3)
-        self.maxpool1 = nn.MaxPool2d(2, 2)
-
-        self.features = nn.Sequential(
-            nn.Conv2d(64, 128, 3),
-            nn.Conv2d(128, 128, 3),
-            nn.ReLU()
-        )
-
-        self.features_list = nn.ModuleList([nn.Linear(128, 256), nn.ReLU()])
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.maxpool1(x)
-        x = self.features(x)
-
-        return x
-
-
-net = MyNet()
-print(net)
-# optimizer = optim.SGD()
+print("hello")
