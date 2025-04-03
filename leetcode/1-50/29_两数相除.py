7ddc247e64bb25bf97e41929d0cf3a72e4d5a81b
eveleaf <eveleaf@outlook.com.ar>
2025-04-03 20:31:33 +0800

    更新

--- leetcode/1-50/29_两数相除.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def divide(self, dividend: int, divisor: int) -> int:
-        ans = int(dividend / divisor)
-        if ans > pow(2, 31) - 1:
-            return pow(2, 31) - 1
-        elif ans < -pow(2, 31):
-            return -pow(2, 31)
-        else:
-            return ans
--- leetcode/51-100/58_最后一个单词的长度.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def lengthOfLastWord(self, s: str) -> int:
-        for item in s.split(' ')[::-1]:
-            if item != '':
-                return len(item)
